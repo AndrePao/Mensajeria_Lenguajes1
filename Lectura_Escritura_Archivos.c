c2a3f4af255b8c33cdf8ffae545073734df80c17
Veronica <ver01raq@gmail.com>
2014-04-01 00:59:27 -0600

    Modificacion, el fork se encuentra en el main, se forma un arreglo de structs que se encuentra alamacenado en memoria

--- Lectura_Escritura_Archivos.c
+++ Lectura_Escritura_Archivos.c
@@ -2,132 +2,72 @@
 //Mètodo imprime la informaciòn del archivo de texto en pantalla
 #include <string.h>
 #include <stdlib.h>
+#include "Ultima_Union.c" //llamada al otro archivo
 //Creacion de un struct para que almacene la informacion del txt
-struct Info_Contacto{
-	char *IP;//variable almacena valores ingresados por usuario
-	char *Puerto;
-	char *Usuario;
+struct InformacionDEContacto{
+	char IP[20];//variable almacena valores ingresados por usuario
+	char Puerto[5];
+	char Usuario[20];
 	};
 
-struct Info_Contacto Lista[100]; // arreglo de struct, para que almacene la info del struct (da problemas)
-int Es_Usuario(char * caracter, char * ptr){ //valida que el usuario que busca se encuentra en el txt
-	if (*caracter!= *ptr)
-	return 0;
+/////////////////////////////////////////////////////////////
+//metodo que obtiene todos los usuarios del txt y los carga a un arreglo
+void Leer_Contactos(struct InformacionDEContacto * ContactosLista){
+	FILE*F; //se crea apuntador
+	F= fopen("ArchivoDeAlmacenamiento", "r+"); //fopenf funciòn permite abrir archivo y r leerlo
+	if (F==NULL) // si no encontro el archivo
+		printf("\nError en archivo\n");
+
 	else{
-	while (*caracter!='-'){
-		if (*caracter== *ptr){
-			caracter++;
-			ptr++;}
-		else
-		return 0;
-	}
-	}
-	return 1;
-
-	}
-/////////////////////////////////////////////////////////////////	
-void Separar_info(char * caracter, int Contador){ // separa la linea de caracteres, y almacena la info en un arreglo de struct
-	char NOM[20];
-	char I[20];
-	char P[20];
-	int c=0;
-	while (*caracter!='-'){
-			NOM[c]=*caracter;
-			caracter++;
-			c++;
-			}
-	NOM[c]='\0';
-	caracter++;
-	c=0;
-	while (*caracter!='-'){
-			I[c]=*caracter;
-			caracter++;
-			c++;
-			}
-	caracter++;
-	I[c]='\0';
-	c=0;
-	while (*caracter!='\0'){
-			P[c]=*caracter;
-			caracter++;
-			c++;}
-	P[c]='\0';
-	Lista[Contador].Usuario=&NOM[0];
-	Lista[Contador].IP=&I[0];
-	Lista[Contador].Puerto=&P[0];
-	printf("\nEstoy en el struct U: %s", Lista[Contador].Usuario);
-	printf("\nEstoy en el struct I: %s", Lista[Contador].IP);
-	printf("\nEstoy en el struct  P: %s",Lista[Contador].Puerto);
-	int puerto= atoi(Lista[Contador].Puerto); //convierte el string a int
-	printf("%d", puerto);
-	}
 	
-////////////////////////////////////////////////////////////////
-void Imprime(FILE * F){ //imprime lista de usuarios
-	char Nombre[30];
-	char * Linea;
-	int c=0;
-	int p=0;
-   char caracter[30];
-	while (feof(F) == 0){ //mientras que no llegue al final del archivo
-		//obtengo hilera
-		fgets(caracter,100,F);
-		//printf("%s",caracter);
-		if ((c>0) && (caracter[0]!='\0')){
-			Linea=&caracter[0];
-			while (*Linea!='-'){
-			Nombre[p]=*Linea;
-			Linea++;
-			p++;
+	int c; //contador que indicara la posicion en la que me encuentro dentro del arreglo de struct
+	char cadena[30]; //variable que almacena las lineas del txt
+	
+		for (c=0;fgets(cadena, 100, F) != NULL;c++){
+			if ((c>0) && (cadena[0]!=0)){
+			
+			int contador=0; //indica por cual caracter del arrego se evalua 
+			int contador2=0; //sirve para asignar el espacio en los arreglos de struct
+			while (cadena[contador]!='-'){ //ingresa el nombre de usuario en el arreglo
+				(ContactosLista[c]).Usuario[contador2] = cadena[contador];
+				contador++;
+				contador2++;
+				}
+			(ContactosLista[c]).Usuario[contador] = '\0'; //agrega un caracter nulo, evito la basura
+			contador2=0;
+			contador++;
+			while (cadena[contador]!='-'){ //ingresa el ip del usuario en el arreglo
+				(ContactosLista[c]).IP[contador2] = cadena[contador];
+				contador++;
+				contador2++;
+				}
+			(ContactosLista[c]).IP[contador2] = '\0';
+			contador2=0;
+			contador++;
+			while (cadena[contador]!='-'){ //ingreso el puerto del usuario en el arreglo
+				(ContactosLista[c]).Puerto[contador2] = cadena[contador];
+				contador++;
+				contador2++;
+				}
+			(ContactosLista[c]).Puerto[contador2] = '\0';
 			}
-		Nombre[p]='\0';
-		printf("\n%s",Nombre);
-		bzero((char*)&Nombre,sizeof(Nombre)); //limpia el arreglo
-				printf("\n%s",Nombre);
+	}
+	(ContactosLista[c]).Usuario[0] = '\0';//ultimo elmento del arreglo es nulo
 
-	}		
-	
+}
+fclose(F); 
+}
 
-	p=0;	
-	c++;
-	}
-		bzero((char*)&Nombre,sizeof(Nombre)); //limpia el arreglo
-	 
-	}
-	
-////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+//metodo q agrega un usuario al txt y carga en memoria la lista de usuarios nuevamente, con el nuevo usuario incorporado
+void Agregar_Contacto(struct InformacionDEContacto * ContactosLista){
+	FILE*F; //se crea apuntador
+	F= fopen("ArchivoDeAlmacenamiento", "r+"); //fopenf funciòn permite abrir archivo y r leerlo
+	if (F==NULL) // si no encontro el archivo
+		printf("\nError en archivo\n");
 
-int leer_Archivo(FILE * F){
-		
-	char N [20];
-			printf("Ingrese nombre usuario ");
-			scanf("%s", N); //obtengo nombre de usuario a buscar
-	char * ptr = &N[0];
-	int c=0;
-   char caracter[30];
-   if (feof(F)!=0) // si el archivo esta vacio
-		return 0;
-   else{
-	while (feof(F) == 0){ //mientras que no llegue al final del archivo
-		//obtengo hilera
-		fgets(caracter,100,F); //obtengo arreglo
-		//printf("%s",caracter);
-		if ((c>0) && (caracter[0]!=0)){ //condicion del txt
-			if(Es_Usuario(&caracter[0], ptr)){ //revisa que el usuario sea el que estoy buscando
-			Separar_info(&caracter[0],c-1);  //metodo que separa informacion del arreglo y envia direccion e IP
-			return 1;//retorna que encontro al usuario
-	}
-	}
-	c++;
-	bzero((char*)&caracter,sizeof(caracter));// se utiliza si uso un arreglo, limpia el arreglo*/	 
-	}
-	}
-	printf("Usuario no encontrado");
-	return 0; //sino encontro al usuario
-	}
-//////////////////////////////////////////////////////////////////////////
-//Mètodo que escribe en el archivo de texto la informaciòn ingresada desde la consola
-int escribir_Archivo(FILE * F){
+	else{
+
 	char IP[20];//variable almacena valores ingresados por usuario
 	char Puerto[20];
 	char Usuario[20];
@@ -146,33 +86,81 @@ int escribir_Archivo(FILE * F){
 	scanf("%s",Puerto);
 	fputs("-",F);
 	fputs(Puerto,F);
-	
-	
+	fputs("-",F); 
 	fputs("\n",F); //cambia de linea el txt
-	return 1;
-}
-////////////////////////////////////////////////////////////////
-int Lectura_Escritura(int opc){ //metodo que abre el archivo
-	FILE*F; //se crea apuntador
-	F= fopen("ArchivoDeAlmacenamiento", "r+"); //fopenf funciòn permite abrir archivo y r leerlo
-	if (F==NULL) // si no encontro el archivo
-		printf("\nError en archivo\n");
-
-	else{
-		if (opc==1) //agrega usuario
-		escribir_Archivo(F);
-		else if (opc==2)
-		leer_Archivo(F); //busca usuario
-		else
-		Imprime(F); //imprime usuarios
 	}
-	fclose(F);  //se cierra el archivo
-	printf("\nListo\n");
-	return 1;
+	Leer_Contactos(ContactosLista);
+	printf("Contacto agregado correctamente\n");
+	fclose(F); 
+}
+
+////////////////////////////////////////////////////////////
+//metodo que busca el nombre del contacto solicitado por el usuario, si lo encuentra retorna el struct, sino lo encuentra retorna el struct vacio
+struct InformacionDEContacto Buscar_Contacto(struct InformacionDEContacto * ContactosLista){
+	char N [20];
+			printf("Ingrese nombre usuario ");
+			scanf("%s", N); //obtengo nombre de usuario a buscar
+	int contador=1;
+	int resultado;
+	while ((ContactosLista[contador]).Usuario[0]!='\0'){
+		resultado=strcmp(N,(ContactosLista[contador]).Usuario);
+		if (resultado==0){
+			printf("\nUsuario encontrado\n ");
+			printf("\nNombre: %s\n ",ContactosLista[contador].Usuario);//imprimo la informacion del contacto
+			printf("\nIP: %s\n ",ContactosLista[contador].IP);
+			printf("\nPuerto: %s\n ",ContactosLista[contador].Puerto);
+			return ContactosLista[contador];
+		}
+		contador++;
+		
+		}
+	
+	return (ContactosLista[contador]);	
+	
 	}
 ////////////////////////////////////////////////////////////
 int main (){
-	//mensaje de que usuario no existe
-	Lectura_Escritura(3);
+	struct InformacionDEContacto ListaDeContactos[30]; //arreglo que contiene structs
+	Leer_Contactos(&ListaDeContactos[0]); //metodo que lee el archivo de texto e ingresa al struct los contactos
+	int opcion;
+	printf("----------------------MENSAJERIA------------------\n");
+	printf("-------------------------MENU---------------------\n\n\nDIGITE\n\n1---->Agregar Contacto\n2---->Iniciar Conversacion\n3---->Imprimir Lista de Contactos\n\nOpcion:  ");
+	scanf("%d",&opcion);
+	if (opcion==1) //si la opcion es agregar un contacto
+		{Agregar_Contacto(&ListaDeContactos[0]);
+		main();
+	}
 	
-	return 1;}
+	else if (opcion==2){ //si la opcion es mensajeria
+		struct InformacionDEContacto UsuarioAContactar=Buscar_Contacto(&ListaDeContactos[0]);//obtengo al contacto
+		if (UsuarioAContactar.Usuario[0]=='\0'){ //verifico que exista el contacto
+			printf("\nUsuario no encontrado, verifique que ingreso correctamente los datos\n ");
+			main();}
+		else{ //si contacto existe
+			//obtengo el puerto, lo convierto a entero
+			int puertoDecontacto=atoi(UsuarioAContactar.Puerto); //convierte el string a int
+			//se realiza la parte de los socktes
+			if (fork())
+				Servidor();
+			else
+				Cliente(puertoDecontacto,UsuarioAContactar.IP);
+			
+			}
+		}
+	
+	else if (opcion==3){ //imprime usuarios en consola
+		int contador;
+		printf("Lista de contactos\n");
+		for (contador=1; ListaDeContactos[contador].Usuario[0]!='\0';contador++){
+			printf("%s \n",ListaDeContactos[contador].Usuario);
+			printf("\n");}
+		main();
+		}
+	
+	else
+	main();
+	
+
+	return 1;
+	}
+
